f0600832d26fa9697295365619833fcc8b986d67
nicolas holland <dutchman@linux.fritz.box>
2019-05-04 09:37:03 +0200

    name was not correct?

--- server.py
+++ server.py
@@ -11,11 +11,11 @@ import pickle
 
 from model import cluster_image
 
-app = Flask(__name__)
+server = Flask(__name__)
 
 model = pickle.load(open('model.pkl', 'rb'))
 
-@app.route('/api', methods=['POST'])
+@server.route('/api', methods=['POST'])
 def predict():
     data = request.get_json(force=True)
 
@@ -30,5 +30,5 @@ def predict():
     return jsonify(retval)
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    server.run(port=5000, debug=True)
 
